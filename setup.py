399259b598ff277ac7a40e65c0f2ca8519414414
Adrian Ruben Dogar <adrian.dogar@gmail.com>
2024-10-31 00:19:14 +0100

    - expose class

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='light_token_manager',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'requests>=2.32.3,<3',
